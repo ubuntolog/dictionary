d8cb1a1ecf26a83c8d46e84ed72faf149dabbd94
Alexandr Chernov <kstchernov@gmail.com>
2019-04-07 21:52:59 +0200

    first commit

---
+++ src/main/java/top/alexcloud/health/AppHealthCheck.java
@@ -0,0 +1,11 @@
+package top.alexcloud.health;
+
+import com.codahale.metrics.health.HealthCheck;
+
+public class AppHealthCheck extends HealthCheck {
+
+    @Override
+    protected Result check() throws Exception {
+        return Result.healthy();
+    }
+}
